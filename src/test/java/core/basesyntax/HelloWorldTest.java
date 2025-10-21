7732d28ad4fab88b1a344820177e54bffc75d7ce
Vitaliy Tishik <vitisik54@gmail.com>
2025-10-21 17:54:29 +0300

    ref

--- src/test/java/core/basesyntax/HelloWorldTest.java
+++ src/test/java/core/basesyntax/HelloWorldTest.java
@@ -6,6 +6,7 @@ import core.basesyntax.model.User;
 import core.basesyntax.service.RegistrationServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
